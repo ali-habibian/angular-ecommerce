3601ff57d7104d6f84eff526689dac4a514f95a2
Ali Habibian <ali.habibian04@gmail.com>
2022-03-03 02:05:43 +0330

    Added cart-details component and implement Shopping Cart Items CRUD

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -43,7 +43,7 @@ export class CartService {
     this.computeCartTotals();
   }
 
-  private computeCartTotals() {
+  public computeCartTotals() {
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
 
@@ -72,5 +72,27 @@ export class CartService {
 
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('-------------')
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity!--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  public remove(theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+
+    // if found remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
   }
 }
